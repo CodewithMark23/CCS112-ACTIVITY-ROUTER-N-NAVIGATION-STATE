194955213711149f5105820e4c7eecd91ec0dc33
Mark Villanueva <markchristianvillanueva23@gmail.com>
2026-08-19 10:18:21 +0800

    feat: made the

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,24 +1,10 @@
 function Footer() {
     return (
-        <footer className="site-footer text-white text-center py-4">
+        <footer className="site-footer text-white text-center py-2">
             <div className="container">
                 <p className="mb-1">
-                    © 2026 My App. All rights reserved.
+                    © 2026 Tian's Portfolio. CCS112.
                 </p>
-
-                <div>
-                    <a href="#" className="text-white text-decoration-none me-3">
-                        Home
-                    </a>
-
-                    <a href="#" className="text-white text-decoration-none me-3">
-                        About
-                    </a>
-
-                    <a href="#" className="text-white text-decoration-none">
-                        Contact
-                    </a>
-                </div>
             </div>
         </footer>
     );
